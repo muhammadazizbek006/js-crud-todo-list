f40a568b80b14ee320141d9512463d10697fe228
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-05 14:30:53 +0300

    x

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const formCreate = document.getElementById('form-create')
 const formEdit = document.getElementById('form-edit')
 const listGroupTodo = document.getElementById('list-group-todo')
-// const messageCreate = document.getElementById('message-create')
+const messageCreate = document.getElementById('message-create')
 const time = document.getElementById('time')
 const modal = document.getElementById('modal')
 const overlay = document.getElementById('overlay')
@@ -11,3 +11,22 @@ const hourEl = document.getElementById('hour')
 const minuteEl = document.getElementById('minute')
 const secondEl = document.getElementById('second')
 const closeEl = document.getElementById('close')
+
+
+
+let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')): [] 
+console.log(todos);
+
+
+formCreate.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        const todoText = formCreate['input-create'].value
+
+console.log(todoText);
+    if(todoText.length){
+        console.log(todoText);
+        
+    }else{
+        messageCreate.textContent = 'plase enter sume text...'
+    }
+})
