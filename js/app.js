a30e8f894303a514d879bc8d7f3bb48f25d6c3f3
muhammadazizbek006 <umarovxaydaraliismonov@gmail.com>
2024-10-05 14:39:05 +0300

    message

--- js/app.js
+++ js/app.js
@@ -1,35 +1,37 @@
-const formCreate = document.getElementById('form-create')
-const formEdit = document.getElementById('form-edit')
-const listGroupTodo = document.getElementById('list-group-todo')
-const messageCreate = document.getElementById('message-create')
-const time = document.getElementById('time')
-const modal = document.getElementById('modal')
-const overlay = document.getElementById('overlay')
+const formCreate = document.getElementById("form-create");
+const formEdit = document.getElementById("form-edit");
+const listGroupTodo = document.getElementById("list-group-todo");
+// const messageCreate = document.getElementById("/");
+const time = document.getElementById("time");
+const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
 /* time elements */
-const fullDay = document.getElementById('full-day')
-const hourEl = document.getElementById('hour')
-const minuteEl = document.getElementById('minute')
-const secondEl = document.getElementById('second')
-const closeEl = document.getElementById('close')
+const fullDay = document.getElementById("full-day");
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minute");
+const secondEl = document.getElementById("second");
+const closeEl = document.getElementById("close");
 
+let todos = JSON.parse(localStorage.getItem("list"))
+  ? JSON.parse(localStorage.getItem("list"))
+  : [];
 
+  function showMessage(where, message){
+    document.getElementById(`${where}`).textContent = message
+    setTimeout(()=>{
+    document.getElementById(`${where}`).textContent = ''
 
-let todos = JSON.parse(localStorage.getItem('list')) ? JSON.parse(localStorage.getItem('list')): [] 
-console.log(todos);
+    }, 2500)
+  }
 
+formCreate.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoText = formCreate["input-create"].value.trim();
 
-formCreate.addEventListener('submit', (e)=>{
-        e.preventDefault()
-        const todoText = formCreate['input-create'].value
-
-console.log(todoText);
-    if(todoText.length){
-        console.log(todoText);
-        
-    }else{
-        messageCreate.textContent = 'plase enter sume text...'
-        setTimeout(()=>{
-            messageCreate.textContent = ''
-        }, 2500)
-    }
-})
+  console.log(todoText);
+  if (todoText.length) {
+    console.log(todoText);
+  } else {
+    showMessage('message-create', 'plase enter some text ...')
+  }
+});
